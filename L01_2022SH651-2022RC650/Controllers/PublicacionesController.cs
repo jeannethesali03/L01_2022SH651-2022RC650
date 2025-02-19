2b598cdeacd2231c56c796047583e1d6d12bbcac
wols616 <wilber.rivas1@catolica.edu.sv>
2025-02-18 18:29:37 -0600

    Controlador de publicación terminado

--- L01_2022SH651-2022RC650/Controllers/PublicacionesController.cs
+++ L01_2022SH651-2022RC650/Controllers/PublicacionesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using L01_2022SH651_2022RC650.Models; 
+using L01_2022SH651_2022RC650.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace L01_2022SH651_2022RC650.Controllers
 {
@@ -15,6 +17,7 @@ namespace L01_2022SH651_2022RC650.Controllers
             _blogContexto = blogContexto;
         }
 
+        //READ
         [HttpGet]
         [Route("GetAll")]
         public IActionResult Get()
@@ -26,6 +29,86 @@ namespace L01_2022SH651_2022RC650.Controllers
                 return NotFound();
             }
             return Ok(listaPublicaciones);
+        }
+
+        //CREATE
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult GuardarPublicacion([FromBody] Publicaciones publicacion)
+        {
+            try
+            {
+                _blogContexto.Publicaciones.Add(publicacion);
+                _blogContexto.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //UPDATE
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public IActionResult ActualizarPublicacion(int id, [FromBody] Publicaciones publicacionModificar)
+        {
+            Publicaciones? publicacionActual = (from e in _blogContexto.Publicaciones where e.publicacionId == id select e).FirstOrDefault();
+
+            if (publicacionActual == null)
+            {
+                return NotFound();
+            }
+
+            publicacionActual.titulo = publicacionModificar.titulo;
+            publicacionActual.descripcion = publicacionModificar.descripcion;
+            publicacionActual.usuarioId = publicacionModificar.usuarioId;
+
+            _blogContexto.Entry(publicacionActual).State = EntityState.Modified;
+            _blogContexto.SaveChanges();
+
+            return Ok(publicacionModificar);
+        }
+
+        //Delete
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+
+        public IActionResult EliminarPublicacion(int id)
+        {
+            Publicaciones? publicacion = (from e in _blogContexto.Publicaciones where e.publicacionId == id select e).FirstOrDefault();
+
+            if (publicacion == null)
+            {
+                return NotFound();
+            }
+            _blogContexto.Publicaciones.Attach(publicacion);
+            _blogContexto.Publicaciones.Remove(publicacion);
+            _blogContexto.SaveChanges();
+            return Ok(publicacion);
+        }
+
+        //PublicacionesPorUsuario
+        [HttpGet]
+        [Route("GetPublicacionesPorUsuario/{id}")]
+        public IActionResult Get(int id)
+        {
+            var publicacion = (from a in _blogContexto.Publicaciones
+                         join u in _blogContexto.Usuarios on a.usuarioId equals u.usuarioId
+                         where u.usuarioId == id
+                         select new
+                         {
+                             u.nombre,
+                             u.apellido,
+                             Publicaciones = (from p in _blogContexto.Publicaciones where p.usuarioId == id select p.titulo).ToList()
+                         }).FirstOrDefault();
+
+
+            if (publicacion == null)
+            {
+                return NotFound();
+            }
+            return Ok(publicacion);
         }
 
     }
