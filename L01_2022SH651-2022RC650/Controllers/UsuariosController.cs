cf3929d20d33d926024b059b25f58122151069f7
jeannethesali03 <eyleen.salinas@catolica.edu.sv>
2025-02-18 18:38:19 -0600

    Todo el controlador de Usuario

--- L01_2022SH651-2022RC650/Controllers/UsuariosController.cs
+++ L01_2022SH651-2022RC650/Controllers/UsuariosController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using L01_2022SH651_2022RC650.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace L01_2022SH651_2022RC650.Controllers
 {
@@ -7,5 +9,125 @@ namespace L01_2022SH651_2022RC650.Controllers
     [ApiController]
     public class UsuariosController : ControllerBase
     {
+        private readonly BlogContext _blogContext;
+        public UsuariosController(BlogContext blogContex)
+        {
+            _blogContext = blogContex;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<Usuarios> listadoUsuario = (from Usuarios in _blogContext.Usuarios select Usuarios).ToList();
+
+            if (listadoUsuario.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoUsuario);
+        }
+
+
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult GuardarUsuario([FromBody] Usuarios usuario)
+        {
+            try
+            {
+                _blogContext.Usuarios.Add(usuario);
+                _blogContext.SaveChanges();
+                return Ok(usuario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public IActionResult ActualizarEquipo(int id, [FromBody] Usuarios usuarioModificar)
+        {
+            Usuarios? usuarioActual = (from Usuarios in _blogContext.Usuarios
+                                  where Usuarios.usuarioId == id
+                                  select Usuarios).FirstOrDefault();
+
+            if (usuarioActual == null)
+            {
+                return NotFound();
+            }
+
+            usuarioActual.rolId = usuarioModificar.rolId;
+            usuarioActual.nombreUsuario = usuarioModificar.nombreUsuario;
+            usuarioActual.nombre = usuarioModificar.nombre;
+            usuarioActual.apellido = usuarioModificar.apellido;
+
+            _blogContext.Entry(usuarioActual).State = EntityState.Modified;
+            _blogContext.SaveChanges();
+
+            return Ok(usuarioModificar);
+        }
+
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult EliminarLibro(int id)
+        {
+            Usuarios? usuarios = (from Usuarios in _blogContext.Usuarios
+                            where Usuarios.usuarioId == id
+                            select Usuarios).FirstOrDefault();
+
+            if (usuarios == null)
+            {
+                return NotFound();
+            }
+
+            _blogContext.Usuarios.Attach(usuarios);
+            _blogContext.Usuarios.Remove(usuarios);
+            _blogContext.SaveChanges();
+
+            return Ok(usuarios);
+        }
+
+        [HttpGet]
+        [Route("GetByNombreApellido")]
+        public IActionResult GetByNombreApellido(string nombre, string apellido)
+        {
+            var UsuarioPorNombre = (from Usuarios in _blogContext.Usuarios
+                                    where Usuarios.nombre.Contains(nombre) && Usuarios.apellido.Contains(apellido)
+                                    select Usuarios).ToList();
+
+
+            if (UsuarioPorNombre == null)
+            {
+                return NotFound();
+            }
+            return Ok(UsuarioPorNombre);
+        }
+
+
+        [HttpGet]
+        [Route("GetByRol")]
+        public IActionResult GetByRol(string rol)
+        {
+            var UsuarioPorNombre = (from Usuarios in _blogContext.Usuarios
+                                    join Roles in _blogContext.Roles on Usuarios.rolId equals Roles.rolId
+                                    where Roles.rol.Contains(rol)
+                                    select Usuarios).ToList();
+
+
+            if (UsuarioPorNombre == null)
+            {
+                return NotFound();
+            }
+            return Ok(UsuarioPorNombre);
+        }
+
+
     }
 }
